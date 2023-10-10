72b8506b3f7a9cab0dabd5f892470d9ed9ca4ab2
deepaksaini2218 <sainimonti17@gmail.com>
2023-10-10 16:21:25 +0530

    added project by anjali

---
+++ ads-it-service/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar(props) {
+    return (
+        <div>
+            <h2>hi i am navbar</h2>
+        </div>
+    );
+}
+
+export default Navbar;
